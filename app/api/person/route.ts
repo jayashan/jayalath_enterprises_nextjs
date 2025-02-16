dee7b7bdef006c4c27470e2bf52ea6c7daccd853
jayashan <jayashan2@live.com>
2025-02-16 22:13:13 +1100

    person

---
+++ app/api/person/route.ts
@@ -0,0 +1,30 @@
+import {NextResponse} from 'next/server'
+import { prisma } from '@/lib/prisma';
+
+export async function POST(req:Request){
+    const {FirstName,LastName,Email,Address,TpNo}=await req.json();
+
+    if(!FirstName || !LastName){
+        return NextResponse.json(
+            {error:'FirstName and LastName are required'},
+            {status:500}
+        );
+    }
+
+    try{
+        const Person=await prisma.person.create({
+            data:{
+                FirstName,
+                LastName,
+                Email,
+                Address,
+                TpNo,
+            },
+        });
+        console.log('Post Created');
+        return NextResponse.json(Person);
+    }catch(error){
+        console.log(error);
+        return NextResponse.json({message:'Could not create post.'})
+    }
+}
