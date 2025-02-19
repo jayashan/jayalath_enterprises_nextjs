26b933dda3c21cdaf37dd9a2ddc8d83648942015
jayashan <jayashan2@live.com>
2025-02-19 12:46:35 +1100

    person params

--- app/api/person/[id]/route.tsx
+++ app/api/person/[id]/route.tsx
@@ -3,10 +3,11 @@ import { NextResponse } from "next/server";
 
 export async function GET(
     req:Request,
-    {params}:{params:{id:string}}
+    {params}:{params:Promise<{id:string}>}
+    
 ){
     try{
-        const id=params.id
+        const id=(await params).id;
         //console.log('id :' , id);
         const person=await prisma.person.findUnique(
             {where:{id}}
