f727ecfa2d2bad33ac8e3d3f517f01253100c5c4
jayashan <jayashan2@live.com>
2025-02-19 12:17:09 +1100

    person details

---
+++ app/api/person/[id]/route.ts
@@ -0,0 +1,20 @@
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET(
+    req:Request,
+    {params}:{params:{id:string}}
+){
+    try{
+        const id=(await params).id
+        //console.log('id :' , id);
+        const person=await prisma.person.findUnique(
+            {where:{id}}
+        );
+        return NextResponse.json(person);
+    
+    }catch(error){
+        console.log(error);
+        return NextResponse.json({message:'Could not fetch person details'});
+    }
+}
